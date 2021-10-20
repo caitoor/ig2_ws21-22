ccf30a6cc770c7239f0b3b1ab90c2244086fc9a1
FR <mail@rauch-fabian.de>
2021-10-20 10:20:38 +0200

    211020 mid-course

--- main.js
+++ main.js
@@ -1,20 +1,39 @@
+const data = [5, 7, 3, 8, 1, 7, 3, 6];
+
 const stage = $("#container");
 const stageHeight = stage.innerHeight();
+const stageWidth = stage.innerWidth();
 
-const data = [5, 7, 3, 8, 3, 6, 7];
+const barWidth = stageWidth / ((data.length * 2) - 1);
 
-const barWidth = 20;
+/* 
+let dataMax = data[0];
+for (let i = 1; i < data.length; i++) {
+    if (data[i] > dataMax) {
+        dataMax = data[i];
+    }
+}
+*/
+
+// einzeiliger kommentar
+
+const dataMax = Math.max(...data); //gibt's auch als Math.min
+console.log(dataMax);
 
 for (let i = 0; i < data.length; i++) {
-    let barHeight = data[i] * 20;
+    let heightCoefficient = stageHeight / dataMax;
+    let barHeight = data[i] * heightCoefficient;
     let xPos = i * 2 * barWidth;
     const myBar = $('<div class="bar"></div>');
     const yPos = stageHeight - barHeight;
     myBar.css({
         width: barWidth,
-        height: barHeight,
+        height: 0,
         left: xPos,
         top: yPos
     });
-    stage.append(myBar);
+    stage.append(myBar); //vgl. prepend
+    myBar.animate({
+        height: barHeight
+    }, 1000);
 }
