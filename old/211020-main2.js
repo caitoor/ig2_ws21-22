54de381f35dbc92c176cd4d65160759ccdfe679a
FR <mail@rauch-fabian.de>
2021-11-16 21:20:14 +0100

    211026

--- old/211020-main2.js
+++ old/211020-main2.js
@@ -1,4 +1,4 @@
-const data = [5, 7, 3, 8, 1, 7, 3, 6, 9, 5, 6, 8, 4, 23, 6, 7];
+const data = [5, 7, 3, 8, 1, 7, 3, 6, 9, 5];
 const stage = $("#container");
 const stageHeight = stage.innerHeight();
 const stageWidth = stage.innerWidth();
@@ -26,7 +26,7 @@ function barDiagram() {
     }
 }
 
-function rain() {
+function stackedDiagram() {
     console.log("zeichne gestapeltes Diagramm");
     const circleHeight = 20;
     const verticalOffset = 5;
@@ -35,9 +35,9 @@ function rain() {
         // i = beim wievielten Wert im Array bin ich gerade?
         for (let j = 0; j < data[i]; j++) {
             //j = wievielter Kreis im aktuellen Balken?    
-            let xPos = (i * 2 * barWidth) - (j *10) ;
+            let xPos = i * 2 * barWidth;
             const myCircle = $('<div class="circle"></div>');
-            let yPos = circleHeight + (j * verticalDistance);
+            let yPos = stageHeight - circleHeight - (j * verticalDistance);
             myCircle.css({
                 width: 20,
                 height: circleHeight,
@@ -47,37 +47,4 @@ function rain() {
             stage.append(myCircle);
         }
     }
-}
-
-function stackedDiagram() {
-    console.log("zeichne gestapeltes Diagramm");
-    const radiusCountX = ((data.length * 2) + (data.length - 1));
-    const radiusCountY = ((dataMax * 2) + (dataMax - 1));
-    // const radiusCount = Math.max(radiusCountX, radiusCountY);
-    const radiusCount = radiusCountX > radiusCountY ? radiusCountX : radiusCountY;
-    const radius = stageWidth / radiusCount;
-    const gap = radius;
-    for (let i = 0; i < data.length; i++) {
-        // i = beim wievielten Wert im Array bin ich gerade?
-        for (let j = 0; j < data[i]; j++) {
-            //j = wievielter Kreis im aktuellen Balken?    
-            let xPos = i * 3 * radius;
-            const myCircle = $('<div class="circle"></div>');
-            let yPos = radius * 3 * j;
-            myCircle.css({
-                width: radius * 2,
-                height: radius * 2,
-                left: xPos,
-                top: yPos
-            });
-            stage.append(myCircle);
-        }
-    }
-}
-
-const myCountry = {
-    longitude: 90,
-    latitude: -45,
-    population: 30000000,
-    countryName: "China"
 }
