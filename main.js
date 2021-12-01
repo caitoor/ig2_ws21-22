84e0dd86c3925784eade26aabed47225eb0c1463
FR <mail@rauch-fabian.de>
2021-12-01 10:45:20 +0100

    211201

--- main.js
+++ main.js
@@ -13,7 +13,9 @@ $(function () {
 
 function prepareData() {
     // merge populationData with positionData
-    positionData.forEach((country) => {
+
+   /*  
+   positionData.forEach((country) => {
         // console.log(country.alpha3Code);
         populationData.forEach((popData) => {
             if (country.alpha3Code === popData.countryCode) {
@@ -23,12 +25,15 @@ function prepareData() {
                 data.push(newCountry);
             }
         });
-    });
+    }); 
+    */
 
-    maxPopulation = getMax(data, "population");
+    data = gmynd.mergeData(positionData, populationData, "alpha3Code", "countryCode");
+
+    maxPopulation = gmynd.dataMax(data, "population");
     console.log("maxPopulation: ", maxPopulation);
 }
-
+/* 
 function getMax(givenData, key) {
     let myMax = givenData[0][key];
     givenData.forEach(element => {
@@ -38,13 +43,14 @@ function getMax(givenData, key) {
     });
     console.log("maximum", key, ": ", myMax);
     return myMax;
-}
+} 
+*/
 
 function drawMap() {
     for (let i = 0; i < data.length; i++) {
         let longitude = gmynd.map(data[i].longitude, -180, 180, 0, stageWidth);
         let latitude = gmynd.map(data[i].latitude, -90, 90, stageHeight, 0);
-        const countryArea = gmynd.map(data[i].population, 1, maxPopulation, 4, 50);
+        const countryArea = gmynd.map(data[i].population, 1, maxPopulation, 30, 100);
         // const countryRadius = Math.sqrt(countryArea / Math.PI);
         const countryRadius = gmynd.circleRadius(countryArea);
         let countryCircle = $("<div></div>");
@@ -52,9 +58,14 @@ function drawMap() {
         countryCircle.css({
             width: countryRadius * 2,
             height: countryRadius * 2,
-            left: longitude-countryRadius,
-            top: latitude-countryRadius
+            left: longitude - countryRadius,
+            top: latitude - countryRadius
         });
+
+        countryCircle.mouseover(function() {
+            countryCircle.addClass("highlight");
+        });
+
         stage.append(countryCircle);
     }
 }
